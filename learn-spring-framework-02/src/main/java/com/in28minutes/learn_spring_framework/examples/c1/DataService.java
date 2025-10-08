f690fd1622f5da63a8a793cf4ac95b8beff35ea0
Xileff <felixsavero78@gmail.com>
2025-10-08 15:57:44 +0700

    move exercise files

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/DataService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/DataService.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learn_spring_framework.exercise;
+package com.in28minutes.learn_spring_framework.examples.c1;
 
 public interface DataService {
     int[] retrieveData();
--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MySQLDataService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MySQLDataService.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learn_spring_framework.exercise;
+package com.in28minutes.learn_spring_framework.examples.c1;
 
 import org.springframework.stereotype.Component;
 
